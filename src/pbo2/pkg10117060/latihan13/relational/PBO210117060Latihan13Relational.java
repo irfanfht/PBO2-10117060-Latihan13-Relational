8a2976515ee36ca6d5021f03cbf3e182924ba758
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 12:33:46 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117060/latihan13/relational/PBO210117060Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117060.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi tentang pertidaksamaan.
+ * yang dimana bil1 dan bil2 sebagai variabel, yang memiliki nilai masing2.
+ */
+public class PBO210117060Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
